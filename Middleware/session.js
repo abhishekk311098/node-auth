1b0adedd2e5c5b327b51c08a11c6777ee2e6b095
abhishek kumar <abhishekk311098@gmail.com>
2024-01-25 02:41:19 +0530

    udpated redis

--- Middleware/session.js
+++ Middleware/session.js
@@ -1,24 +1,25 @@
-const session = require('express-session');
-const redisClient = require('../db/redis');
-const RedisStore = require('connect-redis').default;
+const session = require("express-session");
+const Redis = require("ioredis")
+const RedisStore = require("connect-redis").default;
 
+const redisClient = new Redis(process.env.REDIS_URL)
 
 redisClient.on("error", (err) => {
-    console.error("Redis Error:", err);
-  });
+  console.error("Redis Error:", err);
+});
 
 const sessionHandler = session({
-    store: new RedisStore({client: redisClient}),
-    secret: process.env.SESSION_SECRET,
-    saveUninitialized: false,
-    resave: false, 
-    name:"sessionId",
-    cookie: {
-        secure: false, // if true: only transmit cookie over https
-        httpOnly: true, // if true: prevents client side JS from reading the cookie
-        maxAge: 1000 * 60 * 5, // session max age in milliseconds
-        sameSite: 'lax' // make sure sameSite is not none
-    }
-})
+  store: new RedisStore({ client: redisClient }),
+  secret: process.env.SESSION_SECRET,
+  saveUninitialized: false,
+  resave: false,
+  name: "sessionId",
+  cookie: {
+    secure: false, // if true: only transmit cookie over https
+    httpOnly: true, // if true: prevents client side JS from reading the cookie
+    maxAge: 1000 * 60 * 5, // session max age in milliseconds
+    sameSite: "lax", // make sure sameSite is not none
+  },
+});
 
-module.exports = {sessionHandler}
+module.exports = { sessionHandler };
--- db/redis.js
+++
@@ -1,6 +0,0 @@
-
-const redisClient = require("ioredis").createClient({
-    url: process.env.REDIS_URL,
-  });
-
-module.exports = redisClient;
